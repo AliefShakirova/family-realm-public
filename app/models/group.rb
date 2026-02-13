5a5035e293215f618a4aaa2bbc6d3a2a2e981170
Alief1620 <Alyashakirova16@gmail.com>
2026-02-13 13:19:19 +0100

    Add members associations to Group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,10 @@
 class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User'
 
+  has_many :group_members, dependent: :destroy
+
+  has_many :members, through: :group_members, source: :user
+
   validates :name, presence: true
   validates :privacy, inclusion: { in: %w(private public) }
 end
