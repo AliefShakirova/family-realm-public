9cfd831a675fbe0d87c86ec850739d428be749fa
Alief1620 <Alyashakirova16@gmail.com>
2026-02-16 12:29:27 +0100

    Add uniqueness validation to GroupMember

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,4 +1,6 @@
 class GroupMember < ApplicationRecord
   belongs_to :user
   belongs_to :group
+
+  validates :user_id, uniqueness: { scope: :group_id } #не позволяет одному пользователю добавится в группу дважды
 end
