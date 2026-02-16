f8d3707e08193b1c4216cadf1dc7ab585c91be52
Alief1620 <Alyashakirova16@gmail.com>
2026-02-16 14:15:02 +0100

    Delete line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,6 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   before_action :accept_pending_invite
-  
 
   private
 
