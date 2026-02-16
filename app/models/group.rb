8aa7d518bf927140baf62f0dd7d74be1eba9ba03
Alief1620 <Alyashakirova16@gmail.com>
2026-02-16 12:25:55 +0100

    Add has_many association to Group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,8 +2,9 @@ class Group < ApplicationRecord
   belongs_to :owner, class_name: 'User'
 
   has_many :group_members, dependent: :destroy
-
   has_many :members, through: :group_members, source: :user
+
+  has_many :group_invitations, dependent: :destroy
 
   validates :name, presence: true
   validates :privacy, inclusion: { in: %w(private public) }
