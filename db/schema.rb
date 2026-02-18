9f8e332795a249ba716167750a62dd24b6569b33
Alief1620 <Alyashakirova16@gmail.com>
2026-02-18 16:53:14 +0100

    Add first_name and last_name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_02_18_133132) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_18_151654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -147,6 +147,8 @@ ActiveRecord::Schema[7.1].define(version: 2026_02_18_133132) do
     t.datetime "updated_at", null: false
     t.integer "role"
     t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
