c94b184d3fea207ea85812a76abc67e5b3c3f7a1
Alief1620 <Alyashakirova16@gmail.com>
2026-02-18 16:51:24 +0100

    Finalize many-to-many relationship between Group and User

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -2,5 +2,10 @@ class GroupMember < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
+  # 1. Связь с промежуточной таблицей
+  has_many :group_members, dependent: :destroy
+
+  has_many :users, through: :group_members
+
   validates :user_id, uniqueness: { scope: :group_id } #не позволяет одному пользователю добавится в группу дважды
 end
