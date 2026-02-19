5f0d04c2cbed0315d1f8b4a8200fae60d752bf6e
Alief1620 <Alyashakirova16@gmail.com>
2026-02-19 13:12:54 +0100

    Implement EventsController with ancestor and media support

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,57 @@
+class EventsController < ApplicationController
+  before_action :set_group
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  def index
+    # Сортируем события по дате (от старых к новым)
+    @events = @group.events.order(event_date: :asc)
+  end
+
+  def show
+  end
+
+  def new
+    @event = @group.events.build
+  end
+
+  def edit
+  end
+
+  def create
+    @event = @group.events.build(event_params)
+
+    if @event.save
+      redirect_to group_events_path(@group), notice: "Событие создано!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to group_events_path(@group), notice: "Событие обновлено."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to group_events_path(@group), notice: "Событие удалено."
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def set_event
+    @event = @group.events.find(params[:id])
+  end
+
+  def event_params
+    # ancestor_ids: [] позволяет передавать список выбранных людей
+    params.require(:event).permit(:title, :description, :event_date, :location_id, :location_name, ancestor_ids: [], media: [])
+  end
+end
