36f1b2c07c2c018ea21247e5d861439d140e2b6a
Alief1620 <Alyashakirova16@gmail.com>
2026-02-24 14:31:48 +0100

    Update SMTP port to 465 for Render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,13 +125,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: 587,
+    port: 465,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    tls:                  true
+
   }
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+
+  config.action_mailer.default_url_options = { host: 'family-realm.onrender.com', protocol: 'https' }
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
